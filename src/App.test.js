c0ee18dc5752652a384fa4da02d72fc56046e710
fandytcc <fandytsui@gmail.com>
2018-01-26 14:04:39 +0100

    set up test for batchpage

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,18 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import App from './App'
 import './setupTests'
+import Navigation from './components/UI/Navigation'
+import Routes from './routes'
 
 describe('<App />', () => {
   const app = shallow(<App />)
 
-  it('wraps everything in a div tag', () => {
-    expect(app).toHaveTagName('div')
+  it('wraps everything in a MuiThemeProvider tag', () => {
+    expect(app).toHaveTagName('MuiThemeProvider')
   })
+
+  it('contains a Route', () => {
+    expect(app).toContainReact(<Routes />)
+  })
+
 })
